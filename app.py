43a3972bebc99fe2e1c5b32fae4bc08bd37d000b
danedabomb <danehankamer@gmail.com>
2018-03-12 00:16:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,8 @@
 #from alpha_vantage.timeseries import TimeSeries
 #from pprint import pprint
 
+from alpha_vantage.timeseries import TimeSeries
+
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
