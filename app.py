34279ae8b32bbd3fe6290b64e13eed199bc17e51
danedabomb <danehankamer@gmail.com>
2018-03-11 21:14:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -83,33 +83,32 @@ def getWelcome(req):
     return response
 
 def makeWebhookResult(data):
+  if action == "yahooWeatherForecast":
     query = data.get('query')
     if query is None:
         return {}
-
     result = query.get('results')
     if result is None:
         return {}
-
     channel = result.get('channel')
     if channel is None:
         return {}
-
     item = channel.get('item')
     location = channel.get('location')
     units = channel.get('units')
     if (location is None) or (item is None) or (units is None):
         return {}
-
     condition = item.get('condition')
     if condition is None:
         return {}
-
     # print(json.dumps(item, indent=4))
-
     speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
              ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+  elif action == "input.welcome":
+    speech = str(data)
+  else:
+    speech = str(data)
+        
     print("Response:")
     print(speech)
 
