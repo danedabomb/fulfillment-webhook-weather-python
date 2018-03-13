7c0906cbc845c546ed9e307eb34f344c5d1b2747
danedabomb <danehankamer@gmail.com>
2018-03-12 21:40:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -73,12 +73,12 @@ def processRequest(req):
         result = req.get("result")
         parameters = result.get("parameters")
         symbol = parameters.get("stock_symbol")
-#        baseurl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
-#        url = baseurl + symbol + "&interval=1min&apikey=YBUTHZK2W8IUHVPI"
-#        result = urlopen(yql_url).read()
-#        data = json.loads(result)
-        speechText = symbol + "is currently trading at " + "$179.00."
-        displayText = symbol + "is currently trading at " + "$179.00."
+        baseurl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
+        url = baseurl + symbol + "&interval=1min&apikey=YBUTHZK2W8IUHVPI"
+        result = urlopen(yql_url).read()
+        data = json.loads(result)
+        speechText = symbol + " is currently trading at " + data
+        displayText = symbol + " is currently trading at " + data
         return {
         "speech": speechText,
         "displayText": displayText,
