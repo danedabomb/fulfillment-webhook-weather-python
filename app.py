a1c49a00007a4661cd2f68e524112a64d2ce440c
danedabomb <danehankamer@gmail.com>
2018-03-12 22:59:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,6 @@
 #from alpha_vantage.timeseries import TimeSeries
 #from pprint import pprint
 
-from googlefinance import getQuotes
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
