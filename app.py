341b08d8c6166bc9247c8a9a07ec5270d075f88b
danedabomb <danehankamer@gmail.com>
2018-03-16 01:02:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ def processRequest(req):
         data = json.loads(result)
         data1= data['Time Series (1min)'][time4]['1. open']
         speech = symbol + " is currently trading at " + data1 + "."
-       chart_speech = "Chart for " + symbol
-       chart_url = "https://www.etoro.com/markets/" + symbol + "/chart"
+        chart_speech = "Chart for " + symbol
+        chart_url = "https://www.etoro.com/markets/" + symbol + "/chart"
         if source == 'facebook':
             return {
                 "speech": speech,
