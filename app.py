74d43b7c0c4b97aa9a8ae91fca484be56913acaa
danedabomb <danehankamer@gmail.com>
2018-04-03 20:50:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,34 @@ def processRequest(req):
         data = json.loads(result)
         data1= data['Time Series (1min)'][time4]['1. open']
         speech = symbol + " is currently trading at $" + data1 + "."
+        chart_speech = "Chart for " + symbol
+        chart_url = "https://www.etoro.com/markets/" + symbol + "/chart"
+        if source == 'facebook':
+            return {
+                "speech": speech,
+                "displayText": speech,
+                "source": "apiai-weather-webhook-sample", 
+                "data": {
+                    "facebook": {
+                      "attachment": {
+                        "type": "template",
+                        "payload": {
+                                "template_type":"button",
+                                "text":speech,
+                                "buttons":[
+                                  {
+                                    "type":"web_url",
+                                    "url":chart_url,
+                                    "title":chart_speech,
+                                    "webview_height_ratio": "compact"
+                                  },
+                                ]
+                            }
+                         }
+                    }
+                }
+            }
+        else:
         return {
         "speech": speech,
         "displayText": speech,
